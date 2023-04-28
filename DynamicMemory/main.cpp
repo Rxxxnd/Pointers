bb78e7dbcb96a87e7bf1063c9d6155fea0d2c21e
tehiv <tehiv@ASUS-ROG-DSKTP>
2023-04-28 14:58:19 +0300

    Dymanic Memory;

--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -13,26 +13,22 @@ void main()
 	Print(arr, n);
 	int value;
 	std::cout << "Enter added value: "; std::cin >> value;
-	//1. create buffer array.
-	int* buffer = new int[n + 1];
+	
+	int* buffer = new int[n + 1]; //1. create buffer array.
 
-	//2. copy all elements of array to a buffer array:
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++) //2. copy all elements of array to a buffer array;
 	{
 		buffer[i] = arr[i];
 	}
+	delete[] arr; //3. Deleting pervious array;
 	
-	//3. Deleting pervious array;
-	delete[] arr;
+	arr = buffer; //4. Changing pervious array adress to a new array;
 	
-	//4. Changing pervious array adress to a new array;
-	arr = buffer;
-	// 4.1 
-	buffer = nullptr; // cleaning second pointer. Null-pointer (pointer @ 0);
-	//5. Now can add new values to 'arr';
-	arr[n] = value;
-	//6. Now added new element to array;
-	n++;
+	buffer = nullptr; // 4.1 cleaning second pointer. Null-pointer (pointer @ 0);
+	
+	arr[n] = value; //5. Now can add new values to 'arr';
+	
+	n++; //6. Now added new element to array;
 
 	Print(arr, n);
 	delete[] arr;
